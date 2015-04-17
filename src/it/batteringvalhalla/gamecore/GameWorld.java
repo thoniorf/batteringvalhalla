8e48dad7074b359974fc9547be2270385ac9562a
thoniorf <thoniorf@gmail.com>
2015-04-17 10:35:46 +0200

    Updated folder structure.

---
+++ src/it/batteringvalhalla/gamecore/GameWorld.java
@@ -0,0 +1,9 @@
+package it.batteringvalhalla.gamecore;
+
+public class GameWorld {
+
+	public GameWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/it/batteringvalhalla/gamegui/GameFrame.java
@@ -0,0 +1,34 @@
+package it.batteringvalhalla.gamegui;
+
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+
+	public GameFrame() throws HeadlessException {
+		// TODO Auto-generated constructor stub
+	}
+
+	public GameFrame(GraphicsConfiguration gc) {
+		super(gc);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GameFrame(String title) throws HeadlessException {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GameFrame(String title, GraphicsConfiguration gc) {
+		super(title, gc);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

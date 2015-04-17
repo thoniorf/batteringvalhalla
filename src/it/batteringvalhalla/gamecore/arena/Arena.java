441245afb3d54e54082c54f4f269f515a5456d07
thoniorf <thoniorf@gmail.com>
2015-04-17 10:35:46 +0200

    Updated folder structure. Moved previous /src into old_src

---
+++ src/it/batteringvalhalla/gamecore/arena/Arena.java
@@ -0,0 +1,9 @@
+package it.batteringvalhalla.gamecore.arena;
+
+public class Arena {
+
+	public Arena() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/it/batteringvalhalla/gamegui/GamePanel.java
@@ -0,0 +1,28 @@
+package it.batteringvalhalla.gamegui;
+
+import java.awt.LayoutManager;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+
+	public GamePanel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public GamePanel(LayoutManager layout) {
+		super(layout);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GamePanel(boolean isDoubleBuffered) {
+		super(isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GamePanel(LayoutManager layout, boolean isDoubleBuffered) {
+		super(layout, isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+}

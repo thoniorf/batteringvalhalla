f1b05b945165c66580334f473d85081df8cbc27d
thoniorf <thoniorf@gmail.com>
2015-05-06 12:21:56 +0200

    Added Arena Class

--- src/it/batteringvalhalla/gamecore/arena/Arena.java
+++ src/it/batteringvalhalla/gamecore/arena/Arena.java
@@ -1,12 +1,23 @@
 package it.batteringvalhalla.gamecore.arena;
 
+import java.awt.Graphics;
+
+import javafx.scene.shape.Circle;
+
 public class Arena {
 
-	public Arena() {
+	private Circle edge;
 
+	public Circle getEdge() {
+		return edge;
 	}
 
-	public void paint() {
+	public Arena() {
+		edge = new Circle(68, 200, 500);
+	}
+
+	public void paint(Graphics g) {
+		g.drawOval(68, 200, 500, 500);
 
 	}
 

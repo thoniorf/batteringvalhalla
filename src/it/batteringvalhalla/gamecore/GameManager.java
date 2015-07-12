1715f76591f07161ee2410e4d92e5c1c54ca76f8
thoniorf <thoniorf@gmail.com>
2015-07-13 00:16:48 +0200

    Modified Game Logic.

--- src/it/batteringvalhalla/gamecore/GameManager.java
+++ src/it/batteringvalhalla/gamecore/GameManager.java
@@ -10,17 +10,11 @@ public class GameManager {
 	GameWorld world;
 	GamePanel panel;
 	CollisionHandler collisiondander;
-	int status;
 
 	public GameManager(GamePanel panel) {
 		this.panel = panel;
 		this.world = new GameWorld();
 		this.collisiondander = new CollisionHandler(new Rectangle(1024, 768));
-		init();
-	}
-
-	public int getStatus() {
-		return status;
 	}
 
 	public GameWorld getWorld() {
@@ -28,50 +22,63 @@ public class GameManager {
 	}
 
 	public void init() {
-		status = 1;
+		world.setState(1);
+		world.newMatch(1);
 	}
 
+	private void nextMatch() {
+		try {
+			Thread.sleep(1470);
+			world.setState(1);
+			world.nextMatch();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void endGame() {
+		if (panel.paintRestartPrompt().equals(1)) {
+			panel.resetInput();
+			world.reset();
+			init();
+		} else
+			System.exit(0);
+	}
+
+	// TODO implements game state in replacements of current game logic
 	public void run() {
 		new Thread() {
 			@Override
 			public void run() {
 				super.run();
 				panel.requestFocus();
-				try {
-					if (world.getNext())
-						Thread.sleep(1500);
-				} catch (InterruptedException e1) {
-					e1.printStackTrace();
-				}
-				while (true) {
-					while (status == 1) {
-						panel.getInput();
+				while (world.getState() != 4) {
+					while (world.getState().equals(1)) {
 						world.update();
-						world.zOrder();
 						collisiondander.checkCollisions(world.getObjects());
 						panel.repaint();
+						panel.getInput();
 						try {
-							if (world.getNext())
-								Thread.sleep(1470);
 							Thread.sleep(30);
 						} catch (InterruptedException e) {
 							e.printStackTrace();
 						}
 					}
-					panel.getInput();
+					if (world.getState().equals(3))
+						nextMatch();
+					else if (world.getState().equals(4))
+						endGame();
 					try {
 						Thread.sleep(30);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
-					// TODO pause menu call
+
 				}
+
 			};
 		}.start();
 
-	}
-
-	public void setStatus(int status) {
-		this.status = status;
 	}
 }
--- src/it/batteringvalhalla/gamegui/menu/MainMenu.java
+++ src/it/batteringvalhalla/gamegui/menu/MainMenu.java
@@ -162,7 +162,8 @@ public class MainMenu extends JPanel {
 	}
 
 	@Override
-	public void paint(Graphics g) {
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
 		g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
 				RenderingHints.VALUE_TEXT_ANTIALIAS_ON);

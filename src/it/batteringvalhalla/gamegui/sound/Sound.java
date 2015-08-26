1c35aa4adfdf48a721f824cc0272c6a201abe295
brady994 <eliana_palermiti@libero.it>
2015-08-26 13:22:09 +0200

    Loaded Sound class

--- src/it/batteringvalhalla/gamegui/sound/Sound.java
+++ src/it/batteringvalhalla/gamegui/sound/Sound.java
@@ -2,6 +2,8 @@ package it.batteringvalhalla.gamegui.sound;
 
 import jaco.mp3.player.MP3Player;
 
+import java.applet.Applet;
+import java.applet.AudioClip;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -9,6 +11,8 @@ import java.net.URL;
 public class Sound {
 
 	public static MP3Player ok;
+	public static AudioClip button;
+	public static AudioClip collision;
 
 	public Sound() {
 
@@ -21,12 +25,29 @@ public class Sound {
 					.getResource(
 							"it/batteringvalhalla/assets/sound/background.mp3")
 					.toString()));
+
+			button = Applet
+					.newAudioClip(new URL(
+							Sound.class
+									.getClassLoader()
+									.getResource(
+											"it/batteringvalhalla/assets/sound/button.wav")
+									.toString()));
+
+			collision = Applet.newAudioClip(new URL(Sound.class
+					.getClassLoader()
+					.getResource(
+							"it/batteringvalhalla/assets/sound/collision.wav")
+					.toString()));
+
 		} catch (MalformedURLException e) {
+
 			ok = new MP3Player(new File(Sound.class
 					.getClassLoader()
 					.getResource(
 							"it/batteringvalhalla/assets/sound/background.mp3")
 					.getPath()));
+
 		}
 	}
 }

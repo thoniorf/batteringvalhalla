9deae7c1b84a36c8b67158820146d4859825246e
thoniorf <thoniorf@gmail.com>
2015-10-28 15:21:22 +0100

    Updated GameFrame

--- src/it/batteringvalhalla/gamegui/GameFrame.java
+++ src/it/batteringvalhalla/gamegui/GameFrame.java
@@ -1,41 +1,116 @@
 package it.batteringvalhalla.gamegui;
 
-import it.batteringvalhalla.gamecore.loader.ManagerFilePlayer;
-import it.batteringvalhalla.gamegui.editorActor.EditorPanel;
-import it.batteringvalhalla.gamegui.menu.ExitMenu;
-import it.batteringvalhalla.gamegui.menu.MainMenu;
-import it.batteringvalhalla.gamegui.menu.OptionMenu;
-import it.batteringvalhalla.gamegui.menu.ScoreBoard;
-import it.batteringvalhalla.gamegui.menu.UsernameMenu;
-import it.batteringvalhalla.gamegui.progress.LoadProgress;
-import it.batteringvalhalla.gamegui.sound.Sound;
-
 import java.awt.Dimension;
 import java.awt.HeadlessException;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import it.batteringvalhalla.gamegui.menu.ExitMenu;
+import it.batteringvalhalla.gamegui.menu.MainMenu;
+import it.batteringvalhalla.gamegui.menu.OptionMenu;
+import it.batteringvalhalla.gamegui.menu.ScoreBoard;
+import it.batteringvalhalla.gamegui.menu.UsernameMenu;
+import it.batteringvalhalla.gamegui.progress.LoadProgress;
+import it.batteringvalhalla.gamegui.sound.FileSound;
+import it.batteringvalhalla.gamegui.sound.Sound;
+
 public class GameFrame extends JFrame {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -5257465474614819083L;
-	private Integer screen_width;
-	private Integer screen_height;
-	private Boolean fullscreen;
-	private Dimension resolution;
-	
-	private ManagerFilePlayer mfp;
 
+	public static void main(String[] args) {
+		GameFrame frame = GameFrame.instance();
+		frame.loading();
+		frame.start();
+	}
+
+	FileSound f;
+	private Boolean fullscreen;
 	private JPanel panel;
-		
-	
-	
+	private Dimension resolution;
+
+	private Integer screen_height;
+	private Integer screen_width;
+
+	private static GameFrame frame = null;
+
+	private GameFrame() throws HeadlessException {
+		this.fullscreen = true;
+		this.screen_width = 1024;
+		this.screen_height = 768;
+		this.setResolution(new Dimension(screen_width, screen_height), fullscreen);
+		this.setTitle("Battering Valhalla");
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+
+	}
+
+	public static GameFrame instance() {
+		if (frame == null)
+			return new GameFrame();
+		return frame;
+	}
+
+	public void backMenu() {
+		setPanel(new MainMenu(this));
+
+	}
+
+	public void exit() throws InterruptedException {
+		setPanel(new ExitMenu(this));
+	}
+
+	public void gameStart() throws InterruptedException {
+		setPanel(new GamePanel(this));
+		((GamePanel) panel).getManager().run();
+		f = new FileSound();
+		if ((f.read()).equals("0")) {
+			Sound.menu.stop();
+			Sound.battle.setRepeat(true);
+			Sound.battle.play();
+		} else {
+			Sound.battle.stop();
+		}
+
+	}
+
+	public FileSound getF() {
+		return f;
+	}
+
+	public Boolean getFullscreen() {
+		return fullscreen;
+	}
 
 	public Dimension getResolution() {
 		return resolution;
+	}
+
+	public void loading() {
+		setPanel(new LoadProgress(this));
+		((LoadProgress) panel).run();
+	}
+
+	public void menuStart() {
+		setPanel(new MainMenu(this));
+	}
+
+	public void opt() throws InterruptedException {
+		setPanel(new OptionMenu(this));
+	}
+
+	public void setFullscreen(Boolean fullscreen) {
+		this.fullscreen = fullscreen;
+	}
+
+	public void setPanel(JPanel panel) {
+		this.panel = panel;
+		this.setContentPane(panel);
+		panel.requestFocus();
+		panel.updateUI();
+		this.pack();
+		this.setLocationRelativeTo(null);
 	}
 
 	public void setResolution(Dimension resolution, Boolean fullscreen) {
@@ -46,51 +121,14 @@ public class GameFrame extends JFrame {
 		this.setUndecorated(fullscreen);
 	}
 
-	public Boolean getFullscreen() {
-		return fullscreen;
-	}
-
-	public void setFullscreen(Boolean fullscreen) {
-		this.fullscreen = fullscreen;
-	}
-
-	private void init() {
-		this.fullscreen = true;
-		this.screen_width = 1024;
-		this.screen_height = 768;
-		this.setResolution(
-				new Dimension(this.screen_width, this.screen_height), true);
-
-	}
-
-	public GameFrame() throws HeadlessException {
-		this.init();
-		this.setTitle("Battering Valhalla");
-		this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setVisible(true);
-		mfp= new ManagerFilePlayer();
-	}
-
-	public static void main(String[] args) {
-		GameFrame frame = new GameFrame();
-		frame.loading();
-		frame.start();
-	}
-
-	public void loading() {
-		
-		panel = new LoadProgress(this);
-		this.setContentPane(panel);
-		panel.updateUI();
-		this.pack();
-		this.setLocationRelativeTo(null);
-		((LoadProgress) panel).run();
+	public void showScores() throws InterruptedException {
+		setPanel(new ScoreBoard(this));
 	}
 
 	private void start() {
 		menuStart();
-		if (ManagerFilePlayer.soundOn()) {
+		f = new FileSound();
+		if ((f.read()).equals("0")) {
 			Sound.menu.setRepeat(true);
 			Sound.menu.play();
 		} else {
@@ -100,87 +138,8 @@ public class GameFrame extends JFrame {
 
 	}
 
-	public void menuStart() {
-		panel = new MainMenu(this);
-		this.setContentPane(panel);
-		panel.requestFocus();
-		panel.updateUI();
-		this.pack();
-		this.setLocationRelativeTo(null);
-
-	}
-
-	public void gameStart() throws InterruptedException {
-		panel = new GamePanel(this);
-		this.setContentPane(panel);
-		panel.updateUI();
-		panel.requestFocus();
-		this.pack();
-		this.setLocationRelativeTo(null);
-		((GamePanel) panel).getManager().run();
-		
-		if (ManagerFilePlayer.soundOn()) {
-			Sound.menu.stop();
-			Sound.battle.setRepeat(true);
-			Sound.battle.play();
-		} else {
-			Sound.battle.stop();
-		}
-
-	}
-
-	public void showScores() throws InterruptedException {
-		panel = new ScoreBoard(this);
-		this.setContentPane(panel);
-		panel.updateUI();
-		panel.requestFocus();
-		this.pack();
-		this.setLocationRelativeTo(null);
-	}
-
-	public void exit() throws InterruptedException {
-		panel = new ExitMenu(this);
-		this.setContentPane(panel);
-		panel.updateUI();
-		panel.requestFocus();
-		this.pack();
-		this.setLocationRelativeTo(null);
-	}
-
-	public void opt() throws InterruptedException {
-		panel = new OptionMenu(this);
-		this.setContentPane(panel);
-		panel.updateUI();
-		panel.requestFocus();
-		this.pack();
-		this.setLocationRelativeTo(null);
-	}
-
-	public void Editor() throws InterruptedException {
-		panel = new EditorPanel(this);
-		this.setContentPane(panel);
-		panel.updateUI();
-		panel.requestFocus();
-		this.pack();
-		this.setLocationRelativeTo(null);
-	}
-	public void backMenu() {
-		panel = new MainMenu(this);
-		this.setContentPane(panel);
-		panel.updateUI();
-		panel.requestFocus();
-		this.pack();
-		this.setLocationRelativeTo(null);
-
-	}
-
 	public void userField() {
-		panel = new UsernameMenu(this);
-		this.setContentPane(panel);
-		panel.updateUI();
-		panel.requestFocus();
-		this.pack();
-		this.setLocationRelativeTo(null);
+		setPanel(new UsernameMenu(this));
 		((UsernameMenu) panel).getUserfield().requestFocusInWindow();
 		((UsernameMenu) panel).getUserfield().selectAll();
 

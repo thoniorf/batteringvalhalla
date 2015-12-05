05f90597978a7db58c8ec070b858692e4a2a4c28
thoniorf <thoniorf@gmail.com>
2015-12-05 19:54:07 +0100

    Updated Player class

--- src/it/batteringvalhalla/gamecore/object/actor/Player.java
+++ src/it/batteringvalhalla/gamecore/object/actor/Player.java
@@ -4,7 +4,7 @@ import it.batteringvalhalla.gamegui.editorActor.ImageEditor;
 
 public class Player extends Actor {
 	private static String username;
-	public static Integer score = 0;
+	private static Integer score = 0;
 
 	public Player(int x, int y) {
 		super(x, y, ImageEditor.getIndexTesta(), ImageEditor.getIndexBusto(), ImageEditor.getIndexCapra());

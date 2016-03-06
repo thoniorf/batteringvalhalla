da3faa991e358ab7b0527920fd8e5cc8e85dfb52
thoniorf <thoniorf@gmail.com>
2016-03-06 17:57:53 +0100

    Updated Player
    Now read player appearance settings

--- src/it/batteringvalhalla/gamecore/object/actor/player/Player.java
+++ src/it/batteringvalhalla/gamecore/object/actor/player/Player.java
@@ -2,6 +2,7 @@ package it.batteringvalhalla.gamecore.object.actor.player;
 
 import java.awt.Point;
 
+import it.batteringvalhalla.gamecore.loader.ManagerFilePlayer;
 import it.batteringvalhalla.gamecore.object.actor.AbstractActor;
 
 public class Player extends AbstractActor {
@@ -9,12 +10,12 @@ public class Player extends AbstractActor {
 	protected static String username;
 
 	public Player(Point origin) {
-		super(origin, 0, 0, 0);
+		super(origin, ManagerFilePlayer.getTop(), ManagerFilePlayer.getMid(), ManagerFilePlayer.getBot());
 		score = new Integer(0);
 	}
 
-	public static void incScore() {
-		Player.score += 1;
+	public static void setScore(int s) {
+		Player.score = s;
 	}
 
 	public static String getUsername() {

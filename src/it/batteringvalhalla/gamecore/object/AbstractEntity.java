9e0b0b34a8ab97fe8fbc67ce14df099c56f67456
Antonio Fortino <thoniorf@gmail.com>
2016-03-31 11:48:12 +0200

    Update Abstract Entity
    Update width and height modifier

--- src/it/batteringvalhalla/gamecore/object/AbstractEntity.java
+++ src/it/batteringvalhalla/gamecore/object/AbstractEntity.java
@@ -5,9 +5,9 @@ import java.awt.Rectangle;
 import java.awt.Shape;
 
 public abstract class AbstractEntity implements Entity, Comparable<Entity> {
-	protected static int width = 50;
-	protected static int height = 50;
-
+	private static final long serialVersionUID = -7137071641988898113L;
+	protected int width;
+	protected int height;
 	protected Boolean alive;
 	protected Point origin;
 	protected Shape shape;
@@ -15,6 +15,8 @@ public abstract class AbstractEntity implements Entity, Comparable<Entity> {
 	public AbstractEntity(Point origin) {
 		this.alive = Boolean.TRUE;
 		this.origin = origin;
+		this.width = 50;
+		this.height = 50;
 		this.shape = new Rectangle(origin.x - width / 2, origin.y - height / 2, width, height);
 
 	}
